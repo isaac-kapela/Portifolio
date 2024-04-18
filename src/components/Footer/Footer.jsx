d14c92d1b6f7665ecef7c5e82e9091c9181eb030
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2024-04-18 13:14:01 -0300

    arrumando a footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { IoIosArrowUp } from "react-icons/io";
+import { IoIosArrowUp, FaLinkedin, FaGithub, FaInstagram, FaFacebook } from "react-icons/io";
 
 import * as S from './Footer_Style';
 
@@ -10,7 +10,14 @@ export default function Footer() {
         <p>copyright &copy; 2024 Por Isaac Kapela | todos os direitos reservados.</p>
       </div>
       <div className="footer-icon">
+      
+            <a href="https://www.linkedin.com/in/isaac-kapela-a75141250/" target="_blank" >  <FaLinkedin /> </a>
+            <a href="https://www.instagram.com/isaacdjk/" target="_blank"> <FaInstagram /> </a>
+            <a href="https://github.com/isaac-kapela" target="_blank"> <FaGithub />  </a>
+            <a href="https://www.facebook.com/isaac.kapela.1/" target="_blank">  <FaFacebook /> </a>
+          
         <a href="#"> <IoIosArrowUp /></a>
+
       </div>
     </footer>
     </S.FooterContainer>
