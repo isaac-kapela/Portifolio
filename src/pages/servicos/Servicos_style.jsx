5a4f27502967bd186f3b00e5dda79023c86b7c4f
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2024-04-18 19:41:14 -0300

    deixando a pagina responsividade na pagina de servicços

--- src/pages/servicos/Servicos_style.jsx
+++ src/pages/servicos/Servicos_style.jsx
@@ -85,4 +85,79 @@ export const ServicosContainer = styled.section`
   .span{
     color: black
   }
+
+  .modal-close {
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 10px;
+    cursor: pointer;
+  }
+
+  @media screen and (max-width: 768px) {
+    .servicos-container .servicos-box {
+      flex: 1 1 100%;
+    }
+
+    .servicos h2 {
+      font-size: 2.6rem;
+    }
+
+    .servicos-box h3 {
+      font-size: 2rem;
+    }
+
+    .servicos-box p {
+      font-size: 1.4rem;
+    }
+
+    .servicos-box i {
+      font-size: 2.5rem;
+    }
+
+    .btn {
+      padding: 0.8rem 2.4rem;
+      font-size: 1.4rem;
+    }
+
+    .modal-content {
+      padding: 20px;
+    }
+
+    .servicos-container {
+      gap: 1rem;
+    }
+
+    .servicos-container .servicos-box {
+      padding: 2rem 1rem 3rem;
+    }
+
+    .servicos-container .servicos-box:hover {
+      transform: scale(1.01);
+    }
+
+    .modal-close {
+      padding: 5px;
+    }
+
+    .modal-close i {
+      font-size: 1.6rem;
+    }
+
+    .modal-close:hover {
+      transform: scale(1.1);
+    } 
+
+    .modal-close:hover i {
+      color: #f1c40f;
+    }
+
+    .modal-content {
+      padding: 20px;
+    }
+
+    .modal-content h2 {
+      font-size: 1.8rem;
+    }
+  }
 `;
